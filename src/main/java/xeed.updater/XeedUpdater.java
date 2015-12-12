8521a2e86e2c3a94bc30964bdd69c13ecbb04e45
Erik <xxxsmoizxxx@gmail.com>
2015-12-12 14:21:13 +0100

    Import to git

---
+++ src/main/java/xeed.updater/XeedUpdater.java
@@ -0,0 +1,111 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * xeed_update.java
+ *
+ * Created on 2010-dec-12, 20:02:39
+ */
+package xeed_updater;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Erik
+ */
+public class xeed_update extends javax.swing.JFrame {
+
+    public xeed_update() {
+        initComponents();
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screensize = tk.getScreenSize();
+        this.setLocation(screensize.width / 2 - this.getWidth() / 2, screensize.height / 2 - this.getHeight() / 2); //Bra start pos
+    }
+
+    public void setProgress(int procent, String msg) {
+        jProgressBar1.setValue(procent);
+        lblMsg.setText(msg);
+        jProgressBar1.repaint();
+        lblMsg.repaint();
+    }
+
+    public void setIndeterminate(boolean b) {
+        jProgressBar1.setIndeterminate(b);
+        jProgressBar1.repaint();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jProgressBar1 = new javax.swing.JProgressBar();
+        lblMsg = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        setUndecorated(true);
+
+        jProgressBar1.setForeground(new java.awt.Color(255, 153, 0));
+        jProgressBar1.setMaximum(10000);
+        jProgressBar1.setString("");
+
+        lblMsg.setForeground(new java.awt.Color(255, 255, 255));
+        lblMsg.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        lblMsg.setText("Loading...");
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/xeed_updater/screen.png"))); // NOI18N
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 360, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(lblMsg, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(jLabel1)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(140, 140, 140)
+                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(lblMsg))
+            .addComponent(jLabel1)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(final String args[]) {
+
+        if (args.length < 2) {
+            JOptionPane.showMessageDialog(null, "Error: Arguments missing, please restart XEED manually.");
+            return;
+        }
+
+        xeed_update x = new xeed_update();
+        x.setVisible(true);
+
+        engine en = new engine(args[1], x,args[0]);
+        Thread tmpThread = new Thread(en);
+        tmpThread.start();
+
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JProgressBar jProgressBar1;
+    private javax.swing.JLabel lblMsg;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/main/java/xeed.updater/engine.java
@@ -0,0 +1,143 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xeed_updater;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Erik
+ */
+public class engine implements Runnable {
+
+    final String szUpdateURL;
+    public String szFile = "";
+    private xeed_update xHandle = null;
+    private long lngFileSize = 0;
+
+    public engine(String file, xeed_update x, String url) {
+        szFile = file;
+        xHandle = x;
+        szUpdateURL = url;
+    }
+
+    public void run() {
+
+        if (ApplyUpdate((szUpdateURL))) {
+            xHandle.setProgress(10000, "Done!");
+        } else {
+            xHandle.setProgress(0, "An Error Occured And A Rollback Has Been Performed.");
+            try {
+                Thread.sleep(4000);
+            } catch (Exception e) {
+            }
+        }
+
+        try {
+            Process ps = Runtime.getRuntime().exec(new String[]{"java", "-jar", szFile, "/del_ud:\"" + new File(engine.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getAbsolutePath() + "\""});
+        } catch (Exception e) {
+        }
+        System.exit(0);
+    }
+
+    private boolean ApplyUpdate(String szUrl) {
+
+        File orgFile = null;
+        File newFile = null;
+
+        try {
+
+            xHandle.setProgress(0, "Downloading Update...");
+
+            orgFile = new File(szFile);
+            newFile = new File(szFile + ".bak");
+            newFile.delete();
+            if (!orgFile.renameTo(newFile)) {
+                JOptionPane.showConfirmDialog(null, "XEED couldn't gain write access to your XEED file.\nPlease make sure that other copies of XEED aren't running.\nThen press 'OK'.", "Please close other copies of XEED.", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
+                if (!orgFile.renameTo(newFile)) {
+                    return false;
+                }
+            }
+            orgFile = new File(szFile + ".bak");
+            newFile = new File(szFile);
+            newFile.delete();
+
+            URLConnection urlconUpdate = new URL(szUrl).openConnection();
+            lngFileSize = urlconUpdate.getContentLength();
+
+            BufferedInputStream in = new java.io.BufferedInputStream(urlconUpdate.getInputStream());
+            FileOutputStream fos = new java.io.FileOutputStream(newFile);
+            BufferedOutputStream bout = new BufferedOutputStream(fos, 1024);
+
+            int count;
+            byte data[] = new byte[4096];
+
+            double chunkpercentage = 0;
+            if (lngFileSize != -1) {
+                chunkpercentage = 9000 / (double) (lngFileSize / 4096);
+            } else {
+                xHandle.setIndeterminate(true);
+            }
+
+            int x = 0;
+            while ((count = in.read(data)) >= 0) {
+                bout.write(data, 0, count);
+                data = new byte[4096];
+
+                if (lngFileSize != -1) {
+                    xHandle.setProgress((int) (x * chunkpercentage), "Downloading Update...");
+                }
+                x++;
+            }
+
+            if (lngFileSize == -1) {
+                xHandle.setIndeterminate(false);
+            }
+
+            xHandle.setProgress(9800, "Applying Update...");
+
+            bout.close();
+            fos.close();
+            in.close();
+
+        } catch (Exception e) {
+            orgFile.renameTo(newFile);
+            return false;
+        }
+
+        try {
+            orgFile.delete();
+            return true;
+        } catch (Exception e) {
+            orgFile.renameTo(newFile);
+            return false;
+        }
+    }
+
+    public String[] GetElements(String szData, String szElement) {
+
+        int intEndPos;
+
+        ArrayList<String> szElements = new ArrayList(0);
+
+        String szRet[] = szData.split("<" + szElement + ">");
+        for (int x = 0; x < szRet.length; x++) {
+
+            intEndPos = szRet[x].indexOf("</" + szElement + ">");
+            if (intEndPos != -1) {
+                szElements.add(szRet[x].substring(0, intEndPos));
+            }
+        }
+        String str[] = new String[szElements.size()];
+        szElements.toArray(str);
+
+        return str;
+    }
+}
